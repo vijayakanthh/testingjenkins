af7de548175dac3d327fdac7e1ddf7b95e5ec52a
vijaykanth <103626496+vijayakanthh@users.noreply.github.com>
2022-05-23 18:04:11 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "RUNNING THE SCRIPT NOW.....!!"
+echo "-------------------------------"
+$os=$(cat /etc/lsb-release)
+echo "the os details are: $os"
+echo "--------------------------------"
